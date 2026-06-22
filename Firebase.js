8d07834ef5d0f761d0ef522254fa8a5d70277a85
TejashwiniTambekar <tejashwinit1805@gmail.com>
2026-06-22 21:11:30 +0530

    firebase updated

--- Firebase.js
+++ Firebase.js
@@ -1,19 +1,18 @@
 // firebase.js
+
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyCn1KVlh9y8vpkemabwqcvXY6u4Nw3m8pQ",
-  authDomain: "nirbhaya-b4c51.firebaseapp.com",
-  projectId: "nirbhaya-b4c51",
-  storageBucket: "nirbhaya-b4c51.appspot.com",
-  messagingSenderId: "848173635948",
-  appId: "1:848173635948:android:0c567515cd8ecef2eafb49"
+  apiKey: "AIzaSyA0UgtUxKmp6Fa8SH10v25G-fKXLgBQz6k",
+  authDomain: "nirbhava-33124.firebaseapp.com",
+  projectId: "nirbhava-33124",
+  storageBucket: "nirbhava-33124.firebasestorage.app",
+  messagingSenderId: "269337684261",
+  appId: "1:269337684261:android:c8ae4106323a05858112cc"
 };
 
-
-// Initialize Firebase
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
@@ -21,4 +20,4 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 
-
+export default app;
